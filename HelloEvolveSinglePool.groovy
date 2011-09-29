0d036a3ea000bee8cb33ca936323f58cab6d7e8c
Ben Doerr <tiggerizzy@bendoerr.me>
2011-09-28 23:03:42 -0400

    Still has issues and gets stuck (local maxima???) but according to comments on HN there are lots of other options or other ways to improve this.

--- HelloEvolveSinglePool.groovy
+++ HelloEvolveSinglePool.groovy
@@ -1,33 +1,33 @@
 Number.metaClass.squared = { delegate.power(2) }
 
 random = java.security.SecureRandom.getInstance("SHA1PRNG", "SUN")
-POOLSIZE = 20
-bases = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ !,0123456789'.toCharArray()
+POOLSIZE = 10
+bases = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ !,0123456789'
 target = "Hello, World!"
 
 def genepool = (0..POOLSIZE).collect {
-    def dna = (0..target.length()-1).collect { bases[random.nextInt(bases.length)] }.join('')
+    def dna = (0..target.length()-1).collect { bases[random.nextInt(bases.length())] }.join('')
     [dna: dna, fitness: fitness(dna, target)]
 }
 
 def fitness(source, target) {
     (0..source.length()-1).inject(0) {fitval, i->
-        fitval += (target[i].toCharacter() - source[i].toCharacter()).squared()
+        fitval += (bases.indexOf(target[i]) - bases.indexOf(source[i])).abs()
     }
 }
 
 def mutate(source) {
     def charpos = random.nextInt(source.length())
     def parts = source.toList()
-    parts[charpos] = parts[charpos].next()
-    return parts.join('')
+    parts[charpos] = random.nextInt(2) == 0 ? nextBase(parts[charpos]) : prevBase(parts[charpos])
+    return random.nextInt(5) == 1 ? mutate(parts.join('')) : parts.join('')
 }
 
 def breed(p1, p2) {
     def childDna = p1.dna.toList()
 
-    def start = random.nextInt(p2.dna.length())
-    def stop = random.nextInt(p2.dna.length())
+    def start = random.nextInt(p2.dna.length()-1)
+    def stop = random.nextInt(p2.dna.length()-1)
     if(start > stop) (stop, start) = [start, stop]
     childDna[start..stop] = p2.dna[start..stop]
      
@@ -43,10 +43,10 @@ def randomParent(genepool) {
 def generation = 0
 while(true) {
     generation++
-    genepool.sort { it.fitness }.reverse()
+    genepool = genepool.sort {l,r-> l.fitness <=> r.fitness }
     
-    if(genepool.first().fitness == 0) { break }
-
+    if(genepool[0].fitness == 0) { break }
+    
     def p1 = randomParent(genepool)
     def p2 = randomParent(genepool)
     def child = breed(p1, p2)
@@ -61,3 +61,12 @@ while(true) {
     println "".padRight(15, '-')
 }
 
+def nextBase(def base) {
+    def i = bases.indexOf(base) + 1 
+    bases[i >= bases.length() ? 0 : i]
+}
+
+def prevBase(def base) {
+    def i = bases.indexOf(base) - 1
+    bases[i < 0 ? bases.length() - 1 : i]
+}
