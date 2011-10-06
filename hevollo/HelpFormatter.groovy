1f62c91237932a44b3cd45e88214b7dab019bb21
Ben Doerr <tiggerizzy@bendoerr.me>
2011-10-05 23:47:54 -0400

    A little more refactoring of SystemOptions

---
+++ hevollo/HelpFormatter.groovy
@@ -0,0 +1,9 @@
+import org.apache.commons.cli.HelpFormatter as CommonsHelpFormatter
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+class HelpFormatter extends CommonsHelpFormatter {
+    protected String rtrim(String s) {
+        return s
+    }
+}
--- hevollo/SystemOptions.groovy
+++ hevollo/SystemOptions.groovy
@@ -1,112 +1,81 @@
 @Singleton
 class SystemOptions {
+    static final String OPTIONS_USAGE = "Simulation.groovy [options]"
+    static final String OPTIONS_USAGE_HEADER = """
+${'==Description'.padRight(20,'=')}
+Hevollo is a super simple life simulator. Insipired by simple 'Hello World' evolutionary algorithms/programs.
+
+Organisms exist in populations They only have a few basic attributes: life, strength and color. Each Organism has a dna sequence which is represented as a String of characters (a-zA-Z0-9). These strings are divided up into groups which represent the gene for each trait.
+
+${'==Usage/Details'.padRight(20, '=')}
+There are currently 4 genes: 'baseLongevity', 'strength', 'color', and 'colorPref'. Coding sequences for each of these genes are 10 characters long. To determine the trait that they code for each character (base) is assiged a number based on it's index (a=1, b=2, ..., A=27, ..., 0=53, ..., 9=62). The minimum value a trait could have is 10 while the maximum value a trait can have is 620. When bases are assigned randomly the distribution of trait values is a guassian/normal function with a mean of 315 and a standard deviation of 56.5. The allows us to specify specific ranges and have chance of a specifc encoding be very rare towards the edges and very common towards the center.
+
+${'==Options'.padRight(20, '=')}
+
+"""
+    static final String OPTIONS_USAGE_FOOTER = ""
 
     private OptionAccessor options
 
     private CliBuilder cli
 
-    private Map defaultValues = [
-            population: 100,
-            baseLongevityShortMin: 10,
-            baseLongevityShortMax: 200,
-            baseLongevityAverageMin: 201,
-            baseLongevityAverageMax: 419,
-            baseLongevityLongMin: 420,
-            baseLongevityLongMax: 620,
-            strengthWeakMin: 10,
-            strengthWeakMax: 200,
-            strengthAverageMin: 201,
-            strengthAverageMax: 419,
-            strengthStrongMin: 420,
-            strengthStrongMax: 620,
-            colorPrefNoPrefMin: 201,
-            colorPrefNoPrefMax: 419,
-            colorPrefPrefMin: 10,
-            colorPrefPrefMax: 200,
-            colorPrefStrongPrefMin: 420,
-            colorPrefStrongPrefMax: 620,
-            colorRedMin: 10,
-            colorRedMax: 200,
-            colorOrangeMin: 201,
-            colorOrangeMax: 249,
-            colorYellowMin: 250,
-            colorYellowMax: 299,
-            colorGreenMin: 300,
-            colorGreenMax: 310,
-            colorBlueMin: 311,
-            colorBlueMax: 360,
-            colorIndigoMin: 361,
-            colorIndigoMax: 419,
-            colorVioletMin: 420,
-            colorVioletMax: 620,
-            windowsColors: false,
-    ]
-
-    private Map types = [
-            population: Integer,
-            baseLongevityShortMin: Integer,
-            baseLongevityShortMax: Integer,
-            baseLongevityAverageMin: Integer,
-            baseLongevityAverageMax: Integer,
-            baseLongevityLongMin: Integer,
-            baseLongevityLongMax: Integer,
-            strengthWeakMin: Integer,
-            strengthWeakMax: Integer,
-            strengthAverageMin: Integer,
-            strengthAverageMax: Integer,
-            strengthStrongMin: Integer,
-            strengthStrongMax: Integer,
-            colorPrefNoPrefMin: Integer,
-            colorPrefNoPrefMax: Integer,
-            colorPrefPrefMin: Integer,
-            colorPrefPrefMax: Integer,
-            colorPrefStrongPrefMin: Integer,
-            colorPrefStrongPrefMax: Integer,
-            colorRedMin: Integer,
-            colorRedMax: Integer,
-            colorOrangeMin: Integer,
-            colorOrangeMax: Integer,
-            colorYellowMin: Integer,
-            colorYellowMax: Integer,
-            colorGreenMin: Integer,
-            colorGreenMax: Integer,
-            colorBlueMin: Integer,
-            colorBlueMax: Integer,
-            colorIndigoMin: Integer,
-            colorIngigoMax: Integer,
-            colorVioletMin: Integer,
-            colorVioletMax: Integer,
-            windowsColors: Boolean,
+    private Map availableOptions = [
+            population: [defaultValue: 100, type: Integer, short: 'p', args: 1, argName: 'size'],
+            baseLongevityShortMin: [defaultValue: 10, type: Integer, args: 1, argName: 'bound'],
+            baseLongevityShortMax: [defaultValue: 200, type: Integer, args: 1, argName: 'bound'],
+            baseLongevityAverageMin: [defaultValue: 201, type: Integer, args: 1, argName: 'bound'],
+            baseLongevityAverageMax: [defaultValue: 419, type: Integer, args: 1, argName: 'bound'],
+            baseLongevityLongMin: [defaultValue: 420, type: Integer, args: 1, argName: 'bound'],
+            baseLongevityLongMax: [defaultValue: 620, type: Integer, args: 1, argName: 'bound'],
+            strengthWeakMin: [defaultValue: 10, type: Integer, args: 1, argName: 'bound'],
+            strengthWeakMax: [defaultValue: 200, type: Integer, args: 1, argName: 'bound'],
+            strengthAverageMin: [defaultValue: 201, type: Integer, args: 1, argName: 'bound'],
+            strengthAverageMax: [defaultValue: 419, type: Integer, args: 1, argName: 'bound'],
+            strengthStrongMin: [defaultValue: 420, type: Integer, args: 1, argName: 'bound'],
+            strengthStrongMax: [defaultValue: 620, type: Integer, args: 1, argName: 'bound'],
+            colorPrefNoPrefMin: [defaultValue: 201, type: Integer, args: 1, argName: 'bound'],
+            colorPrefNoPrefMax: [defaultValue: 419, type: Integer, args: 1, argName: 'bound'],
+            colorPrefPrefMin: [defaultValue: 10, type: Integer, args: 1, argName: 'bound'],
+            colorPrefPrefMax: [defaultValue: 200, type: Integer, args: 1, argName: 'bound'],
+            colorPrefStrongPrefMin: [defaultValue: 420, type: Integer, args: 1, argName: 'bound'],
+            colorPrefStrongPrefMax: [defaultValue: 620, type: Integer, args: 1, argName: 'bound'],
+            colorRedMin: [defaultValue: 10, type: Integer, args: 1, argName: 'bound'],
+            colorRedMax: [defaultValue: 200, type: Integer, args: 1, argName: 'bound'],
+            colorOrangeMin: [defaultValue: 201, type: Integer, args: 1, argName: 'bound'],
+            colorOrangeMax: [defaultValue: 249, type: Integer, args: 1, argName: 'bound'],
+            colorYellowMin: [defaultValue: 250, type: Integer, args: 1, argName: 'bound'],
+            colorYellowMax: [defaultValue: 299, type: Integer, args: 1, argName: 'bound'],
+            colorGreenMin: [defaultValue: 300, type: Integer, args: 1, argName: 'bound'],
+            colorGreenMax: [defaultValue: 310, type: Integer, args: 1, argName: 'bound'],
+            colorBlueMin: [defaultValue: 311, type: Integer, args: 1, argName: 'bound'],
+            colorBlueMax: [defaultValue: 360, type: Integer, args: 1, argName: 'bound'],
+            colorIndigoMin: [defaultValue: 361, type: Integer, args: 1, argName: 'bound'],
+            colorIndigoMax: [defaultValue: 419, type: Integer, args: 1, argName: 'bound'],
+            colorVioletMin: [defaultValue: 420, type: Integer, args: 1, argName: 'bound'],
+            colorVioletMax: [defaultValue: 620, type: Integer, args: 1, argName: 'bound'],
+            windowsColors: [defaultValue: false, type: Boolean, desc: 'Disable colors for windows'],
     ]
 
     SystemOptions() {
-        String header = """\n
-Description
-=========================================================================
-Hevollo is a super simple life simulator. Insipired by simple 'Hello World' evolutionary algorithms.
-+
-Organisms exist in populations They only have a few basic attributes: life, strength and color. Each Organism has a dna sequence which is represented as a String of characters (a-zA-Z0-9). These strings are divided up into groups which represent the gene for each trait.
-+
-Usage
-=========================================================================
-Other than the options described below.
-+
-Configure chance of trait
--------------------------------------------------------------------------
-There are currently 4 traits: 'baseLongevity', 'strength', 'color', and 'colorPref' coding sequences for each of these traits are 10 characters long. To determine the trait that they code for each character (base) is assiged a number based on it's index (a=1, b=2, ..., A=27, ..., 0=53, ..., 9=62). The minimum value a trait could have is 10 while the maximum value a trait can have is 620. The reason for this is to form a gaussian/normal function with a mean of 315 and a standard deviation of 56.5. The allows us to specify specific ranges and have chance of a specifc encoding be very rare towards the edges and very common towards the center.
-+
-Options
--------------------------------------------------------------------------
-"""
-        Map usage = [
-                usage: 'Simulation [options]',
-                header: header,
-                footer: 'Test footer']
-        cli = new CliBuilder(usage)
+        cli = new CliBuilder(usage: OPTIONS_USAGE, header: OPTIONS_USAGE_HEADER, footer: OPTIONS_USAGE_FOOTER)
+        cli.formatter = new HelpFormatter()
+        cli.width = 110
 
         cli.h longOpt: 'help', 'Prints this help message'
-        cli._ longOpt: 'population', args: 1, argName: 'size', 'The size of the initial population. Default is 100'
-        cli._ longOpt: 'windowsColors', 'Use this option to hide color escape codes on windows when using one of the console displays. The dos console doesn\'t support ansi escape sequences.'
+
+        availableOptions.each {k,v->
+            Map usageMap = [longOpt: k]
+            if(v.args) usageMap.args = v.args
+            if(v.argName) usageMap.argName = v.argName
+
+            String desc = v.desc ?: "Default Value: ${v.defaultValue}, ".padRight(20) + "Type: ${v.type.getSimpleName()}"
+
+            if(v.short) {
+                cli."${v.short}"(usageMap, desc)
+            } else {
+                cli._(usageMap, desc)
+            }
+        }
     }
 
     void initialize(String[] args) {
@@ -115,13 +84,19 @@ Options
     }
 
     Object getAt(String name, Object defaultValue = null) {
-        getProperty(name) ?: (defaultValue ?: defaultValues.get(name))
+        getProperty(name) ?: (defaultValue ?: availableOptions.get(name).defaultValue)
     }
 
     Object getProperty(String name) {
+        if(name == 'cli') return cli
+
         Object prop = options.getProperty(name)
-        if (prop && types.containsKey(name)) prop.asType(types.get(name))
-        else prop
+
+        if (prop && availableOptions.containsKey(name)) {
+            return prop.asType(availableOptions.get(name).type)
+        }
+
+        return prop
     }
 
     Boolean isInvalid() {
